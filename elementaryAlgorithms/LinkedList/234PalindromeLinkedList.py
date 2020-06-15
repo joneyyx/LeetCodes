c01c7e224c9a7dc547e74199afaee00d0b7111e3
Victor.Zhong <rb813@cummins.com>
2020-06-15 23:56:20 +0800

    20200615 commit

--- elementaryAlgorithms/LinkedList/234PalindromeLinkedList.py
+++ elementaryAlgorithms/LinkedList/234PalindromeLinkedList.py
@@ -8,3 +8,44 @@
 #
 # Input: 1->2->2->1
 # Output: true
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        pre = None
+        if not head or not head.next:
+            return True
+        else:
+            slow = head
+            fast = head.next
+
+        while fast and fast.next:
+            tmp = slow
+            slow = slow.next
+            fast = fast.next.next
+            tmp.next = pre
+            pre = tmp
+
+        # 此处是精髓：奇偶情况下分别拆成两个链表
+        if fast:
+            fast = slow.next
+            slow.next = pre
+        else:
+            fast =  slow.next
+            slow = pre
+
+        # compare
+        while fast and slow:
+            if fast.val != slow.val:
+                return False
+            else:
+                fast = fast.next
+                slow = slow.next
+
+        return  True
